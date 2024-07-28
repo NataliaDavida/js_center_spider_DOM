3b361a011298d05e52be61cc20828ff3df24a927
NataliaDavida <nataliadavida88@gmail.com>
2024-07-28 09:12:48 +0300

    make build

---
+++ index.95b5035f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=Math.round(e.clientHeight/2-t.clientHeight/2)+"px",t.style.left=Math.round(e.clientWidth/2-t.clientWidth/2)+"px";
+//# sourceMappingURL=index.95b5035f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,7 @@
+"use strict";
+const wallEl = document.querySelector(".wall");
+const spiderEl = document.querySelector(".spider");
+spiderEl.style.top = Math.round(wallEl.clientHeight / 2 - spiderEl.clientHeight / 2) + "px";
+spiderEl.style.left = Math.round(wallEl.clientWidth / 2 - spiderEl.clientWidth / 2) + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
